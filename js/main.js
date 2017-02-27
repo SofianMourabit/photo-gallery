f68aa7bc98d5141d4f274865a25771629e2e2038
Sofian Mourabit <smourabit@informationmapping.com>
2017-02-27 16:30:41 +0700

    Added search functionality for gallery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 baguetteBox.run('.gallery', {
     // Custom options
 });
+var options = {
+    valueNames: [
+      { name: 'item', attr: 'title' }
+    ]
+};
+
+var galleryList = new List('gallery', options);
