9fe9b0554a59285ce8becc8d50450f732a677182
Sofian Mourabit <smourabit@informationmapping.com>
2017-02-28 18:56:19 +0700

    Created jQuery search box

--- js/main.js
+++ js/main.js
@@ -1,10 +1,40 @@
+// Lightbux Plugin (Vanilla JS)
 baguetteBox.run('.gallery', {
     // Custom options
 });
-var options = {
+
+// Search filter (jQuery plugin)
+/* var options = {
     valueNames: [
       { name: 'item', attr: 'title' }
     ]
 };
 
 var galleryList = new List('gallery', options);
+*/
+
+// Extra credit : Create a search box in jQuery
+/*
+  1. Get Input box by id and value for each change
+  2. For each anchor element Get title attributes
+  3. If InputText can't be found in captionText hide the parent list item
+*/
+
+$( "#search" ).keyup(function() {
+    var inputText= "";
+    inputText = $(this).val().toLowerCase();
+
+    $( ".item" ).each(function( index ) {
+        var captionText = $( this ).attr("title").toLowerCase();
+
+        if (!~captionText.indexOf(inputText)){
+          $(this).parent().hide();
+          console.log("you are a genius");
+        }else {
+          $(this).parent().show();
+        }
+     // console.log( index + ": " + $( this ).attr("title") );
+    });
+
+    console.log(inputText);
+});
